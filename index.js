eaaf3b1da1dd0a617637cdc80eb9a60d7cc20d6b
Gabriel Duarte <gabrielduarte_jardim@hotmail.com>
2022-04-27 23:32:52 -0300

    Keydown

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Funções de botões click
+
 let display = document.getElementById("display");
 let buttons = Array.from(document.getElementsByClassName("button"));
 
@@ -24,3 +26,50 @@ buttons.map((button) => {
     }
     });
 });
+
+//Funções de teclado
+const numbersEl = document.querySelectorAll(".button");
+
+document.addEventListener('keydown', (e) => {
+    if(
+        e.key === '0' ||
+        e.key === '1' ||
+        e.key === '2' ||
+        e.key === '3' ||
+        e.key === '4' ||
+        e.key === '5' ||
+        e.key === '6' ||
+        e.key === '7' ||
+        e.key === '8' ||
+        e.key === '9' ||
+        e.key === '.' ||
+        e.key === '*' ||
+        e.key === '-' ||
+        e.key === '+' ||
+        e.key === '(' ||
+        e.key === ')' ||
+        e.key === '/'
+    ){
+        clickButtonEl(e.key);
+    } else if (e.key == "Enter" || e.key === "=") {
+        clickEqual();
+    } else if (e.key === "←") {
+        clickDel();
+    }
+});
+
+function clickButtonEl(key) {
+    numbersEl.forEach((button) => {
+    if (button.innerText === key) {
+        button.click();
+    }
+    });
+}
+
+function clickEqual() {
+    equal.click();
+}
+
+function clickDel() {
+    del.click();
+}
